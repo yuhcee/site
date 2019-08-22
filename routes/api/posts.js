fea572af15e33e8f9b02a1a889609ebf89f440ee
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-08-22 16:16:24 +0100

    Created Post route and validation.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+//  Post model
+const Post = require('../../models/Post');
+
+// Validation
+const validatePostInput = require('../../validation/post');
 
 /**
  * @route   GET api/posts/test
@@ -8,5 +16,30 @@ const router = express.Router();
  * @access  Pubic
  */
 router.get('/test', (req, res) => res.json({ message: 'Posts Works' }));
+
+/**
+ * @route   POST api/posts/test
+ * @desc    Create post
+ * @access  Private
+ */
+
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validatePostInput(req.body);
+  //  Check Validation
+  if (!isValid) {
+    //  If any errors, send 400 with errors object
+    return res.status(400).json(errors);
+  }
+
+  const { text, name, avatar } = req.body;
+  const newPost = new Post({
+    text,
+    name,
+    avatar,
+    user: req.user.id,
+  });
+
+  return newPost.save().then(post => res.json(post));
+});
 
 module.exports = router;
