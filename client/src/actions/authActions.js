7f25b35b4332f9419828d681ea2c4244acc5a272
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-11-20 00:12:05 +0100

    Add authAction for user registration.

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { setAuthToken } from "../utils/setAuthToken";
+
+import { GET_ERRORS } from "./types";
+
+// Register USer
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post("/api/users/register", userData)
+    .then(res => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+
